a9ccee022f1d4e30eee0a631083780cea8fc1a96
kmal808 <kjmalley7@gmail.com>
2023-01-15 00:42:01 -1000

    task: adding script info and metadata to file

--- lux.10s.sh
+++ lux.10s.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+#* Display the value read from a tsl2591 I2C lux sensor => https://www.adafruit.com/product/1980
+#
+# *by Kurt Malley (k@kmal.rip)
+
+#! metadata
+#? <xbar.title>Lunar Sensor</xbar.title>
+#? <xbar.version>0.0.1</xbar.version>
+#? <xbar.author.github>kmal808</xbar.author.github>
+#? <xbar.desc>Display the value read from a tsl2591 I2C lux sensor</xbar.desc>
+#? <xbar.image>Lunar Sensor</xbar.image>
+
 LUX_VALUE=$(curl lunarsensor.local/sensor/ambient_light_tsl2591 | jq -r .state)
 
-echo "☀️ $LUX_VALUE lux" 
+echo "☀️ $LUX_VALUE lx" 
